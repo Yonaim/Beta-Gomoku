442c798ac9d63f99aec92997b19491cc26fb9579
yonaMac <yonaimyo@gmail.com>
2025-05-16 05:21:42 +0900

    Refactor: Use macro value instead of embedded

--- src/ui/console_renderer.py
+++ src/ui/console_renderer.py
@@ -1,23 +1,25 @@
 import numpy as np
 from game.constants import BLACK, WHITE
+from game.settings import BOARD_LENGTH
 
 EMPTY_CHAR = "·"
 BLACK_CHAR = "●"
 WHITE_CHAR = "○"
 
 class ConsoleRenderer:
-    def __init__(self, size: int = 15):
+
+    def __init__(self, size: int = BOARD_LENGTH):
         self.size = size
 
     def draw(self, board: np.ndarray):
         for y in range(self.size):
-                    row = []
-                    for x in range(self.size):
-                        p = board[y, x]
-                        row.append(
+            row = []
+            for x in range(self.size):
+                p = board[y, x]
+                row.append(
                             BLACK_CHAR if p == BLACK
                             else WHITE_CHAR if p == WHITE
                             else EMPTY_CHAR
                         )
-                    print(" ".join(row))
+            print(" ".join(row))
         print()  # blank line
