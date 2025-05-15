a0ec31434ff07afb9abb015b5f06a368a9dec63f
yonaMac <yonaimyo@gmail.com>
2025-05-16 04:28:31 +0900

    Feat: Add settings macro

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,5 @@
 BOARD_LENGTH = 15
 N_ITERATION = 1500
-TIME_LIMIT = 3
+TIME_LIMIT = 10
+N_ROLLOUT = 10  # Number of rollouts (playouts) to average in a single simulation
+MAX_DEPTH = 20  # Maximum number of moves per rollout (rollout depth limit)
