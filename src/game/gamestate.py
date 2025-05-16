77cf9754cbe38bcc6c8c55b666bab4c81b95b753
yonaMac <yonaimyo@gmail.com>
2025-05-16 16:35:07 +0900

    Perf: save calculated terminal information

--- src/game/gamestate.py
+++ src/game/gamestate.py
@@ -1,39 +1,39 @@
 from __future__ import annotations
 import numpy as np
 import copy
-from .constants import WIN_STONE_CNT, EMPTY, DIRS
+
+from ui import console_renderer
+from .constants import WIN_STONE_CNT, EMPTY, DIRS, WHITE
 from .settings import BOARD_LENGTH
+from typing import Optional
 
 class GameState:
-	board: np.ndarray
-	current_player: int
+    board: np.ndarray
+    current_player: int
+    last_move: tuple[int, int] | None
+    is_terminal: bool
+    terminal_winner: int | None
 
-	def __init__(self, current_player: int):
-		self.board = np.zeros((BOARD_LENGTH,BOARD_LENGTH), dtype=np.uint8)
-		self.current_player = current_player
-	
-	def get_legal_moves(self) -> list[tuple[int, int]]:
-		arr = np.argwhere(self.board == EMPTY)
-		return [tuple(pos) for pos in arr]
- 
-	def apply_move(self, move: tuple[int, int]):
-		if self.board[move] != EMPTY:
-			raise ValueError
-		self.board[move] = self.current_player
+    def __init__(self, current_player: int):
+        self.board = np.zeros((BOARD_LENGTH, BOARD_LENGTH), dtype=np.uint8)
+        self.current_player = current_player
+        self.last_move = None
+        self.is_terminal = False
+        self.terminal_winner = None
 
-	def get_winner(self) -> int:
-		for y in range(BOARD_LENGTH):
-			for x in range(BOARD_LENGTH):
-				player = self.board[y][x]
-				if (player == EMPTY):
-					continue
-				for dx, dy in DIRS:
-					if self._count_stone_in_dir(x, y, dx, dy) >= WIN_STONE_CNT:
-						return player
-		return EMPTY
+    def get_legal_moves(self) -> list[tuple[int, int]]:
+        arr = np.argwhere(self.board == EMPTY)
+        return [tuple(pos) for pos in arr]
 
-	def is_terminated(self) -> bool:
-		return self.get_winner() != 0 or len(self.get_legal_moves()) == 0
+    def apply_move(self, move: tuple[int, int]):
+        if self.board[move] != EMPTY:
+            raise ValueError
+        self.board[move] = self.current_player
+        self.last_move = move
+        winner = self.winner_by_last_move()
+        if winner != None or len(self.get_legal_moves()) == 0:
+            self.is_terminal = True
+            self.terminal_winner = winner
 
 	# internal -----------------------------------------------------------------
 
