ce15fa628301f0396d495180109ab28fa66d428c
yonaMac <yonaimyo@gmail.com>
2025-05-16 16:37:07 +0900

    Perf: replace logic for check winner with only last move

--- src/game/gamestate.py
+++ src/game/gamestate.py
@@ -35,8 +35,44 @@ class GameState:
             self.is_terminal = True
             self.terminal_winner = winner
 
-	# internal -----------------------------------------------------------------
+    def winner_by_last_move(self) -> Optional[int]:
+        if self.last_move == None:
+            return None
+        y, x = self.last_move
+        player = self.board[y, x]
+        for dx, dy in DIRS:
+            count = (
+                self._count_one_dir(x, y, dx, dy)
+                + 1
+                + self._count_one_dir(x, y, -dx, -dy)
+            )
+            if count >= WIN_STONE_CNT:
+                return player
+        return None
 
+    # internal -----------------------------------------------------------------
+
+    def _count_one_dir(self, x: int, y: int, dx: int, dy: int) -> int:
+        cnt = 0
+        nx, ny = x + dx, y + dy
+        player = self.board[y, x]
+        
+        if (player == EMPTY):
+            return 0
+        
+        while (
+            (0 <= nx < BOARD_LENGTH)
+            and (0 <= ny < BOARD_LENGTH)
+            and self.board[ny, nx] == player
+        ):
+            cnt += 1
+            nx += dx
+            ny += dy
+        return cnt
+
+    def clone(self) -> GameState:
+        return copy.deepcopy(self)
+	
 	# for debug
     def print_board(self) -> None:
         renderer = console_renderer.ConsoleRenderer(BOARD_LENGTH)
