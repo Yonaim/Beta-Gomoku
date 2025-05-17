2e72e30601ceeb6ce1997ba65631f90a8aaf9fd5
yonaMac <yonaimyo@gmail.com>
2025-05-17 18:31:06 +0900

    Perf: draw bitset board

--- src/ui/console_renderer.py
+++ src/ui/console_renderer.py
@@ -2,25 +2,30 @@ import numpy as np
 
 from constants import BLACK, WHITE
 from settings import BOARD_LENGTH
+from src.game.bitset import bit
 
 EMPTY_CHAR = "·"
 BLACK_CHAR = "●"
 WHITE_CHAR = "○"
+
 
 class ConsoleRenderer:
 
     def __init__(self, size: int = BOARD_LENGTH):
         self.size = size
 
-    def draw(self, board: np.ndarray):
+    def draw(self, occupy_bitset: int, color_bitset: int): 
         for y in range(self.size):
             row = []
             for x in range(self.size):
-                p = board[y, x]
+                occ = bit(occupy_bitset, x, y)
+                if occ is 1:
+                    continue
+                color = bit(color_bitset, x, y)
                 row.append(
-                            BLACK_CHAR if p == BLACK
-                            else WHITE_CHAR if p == WHITE
-                            else EMPTY_CHAR
-                        )
+                    BLACK_CHAR
+                    if color == BLACK
+                    else WHITE_CHAR if color == WHITE else EMPTY_CHAR
+                )
             print(" ".join(row))
         print()  # blank line
