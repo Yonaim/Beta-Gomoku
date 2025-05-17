6af730608d8ea05e43ad508546ae2a7b3e6b6201
yonaMac <yonaimyo@gmail.com>
2025-05-18 01:36:24 +0900

    Test: Change default settings

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
-BOARD_LENGTH = 15
+BOARD_LENGTH = 10
 N_ITERATION = 1500
 TIME_LIMIT = 3
-N_ROLLOUT = 10  # Number of rollouts (playouts) to average in a single simulation
+N_ROLLOUT = 5  # Number of rollouts (playouts) to average in a single simulation
 MAX_DEPTH = 20  # Maximum number of moves per rollout (rollout depth limit)
 DEBUG_MODE = False
